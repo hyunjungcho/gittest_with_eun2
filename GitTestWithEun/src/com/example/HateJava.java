235561a6e738bec08a575ca0e5f3556f64ef4383
1-718-8 <1-718-8@210.125.213.47>
2016-09-02 15:56:17 +0900

    init4

--- GitTestWithEun/src/com/example/HateJava.java
+++ GitTestWithEun/src/com/example/HateJava.java
@@ -5,6 +5,9 @@ public class HateJava {
 	public static void main(String[] args) {
 		System.out.println("어려워!!!!!!!!");
 		System.out.println("응답해!!!경은!!!!");
+		
+		
+		System.out.println("나는 팀원이다!!!!!!!!!!!!으아!!!!!!!");
 	}
 
 }
